1e37939697a2f530ca3f0382220fd544ed9bdd4f
念响 <qihao.xqh@antfin.com>
2018-12-09 17:27:42 +0800

    feat: 初始化脚手架

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(Mint);
 
 new Vue({
   router,
